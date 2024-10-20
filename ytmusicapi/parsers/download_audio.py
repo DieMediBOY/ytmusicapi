6b0895ce9a1b2765968a66ba09105c7ea6a958f0
DieMediBOY <79429508+DieMediBOY@users.noreply.github.com>
2024-10-20 17:07:00 -0500

    Update download_audio.py

--- ytmusicapi/parsers/download_audio.py
+++ ytmusicapi/parsers/download_audio.py
@@ -1,5 +1,4 @@
 import yt_dlp
-import json
 import sys
 import os
 
@@ -26,22 +25,22 @@ def download_audio(youtube_id):
             ydl.download([youtube_url])
 
         mp3_filename = f"{youtube_id}.mp3"
-        
         # Verificar que el archivo MP3 existe
         if os.path.exists(mp3_filename):
-            return {"status": "success", "file": mp3_filename}
+            return mp3_filename
         else:
-            return {"status": "error", "message": "File not found after download."}
+            raise FileNotFoundError("File not found after download.")
 
     except Exception as e:
-        # Devuelve un JSON con el error
-        return {"status": "error", "message": str(e)}
+        raise e
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(json.dumps({"error": "No YouTube ID provided"}))
+        print("No YouTube ID provided")
     else:
         youtube_id = sys.argv[1]
-        result = download_audio(youtube_id)
-        # Asegurarse de que solo imprimimos JSON válido
-        print(json.dumps(result))
+        try:
+            result = download_audio(youtube_id)
+            print(result)
+        except Exception as e:
+            print(str(e))
